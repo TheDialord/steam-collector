0260dd2ca4d5b4bf4a7232c54483aec4121c5f41
OpenShift System User <55635cc55004460c9a00017a@ex-std-node534.prod.rhcloud.com>
2015-05-25 14:37:16 -0400

    initial wsgi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='SteamCollector',
+      version='1.0',
+      description='SteamCollector App',
+      author='Yegordia',
+      author_email='steam@steamcommunity.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask'],
+     )
