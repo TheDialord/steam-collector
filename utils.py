fcc95cc158f8a65bd13b65f21131dbbdcb0f3f91
Yegor Dia <yegordia@gmail.com>
2015-05-26 02:30:34 +0500

    th added

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import fileinput
+import os
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 def open_file_line(path):
-    with open(path, "r") as ins:
+    with open(os.path.join(APP_ROOT, path), "r") as ins:
         for line in ins:
             return line
-        with open(path, 'w') as fout:
+        with open(os.path.join(APP_ROOT, path), 'w') as fout:
             fout.writelines(ins[1:])
 
     return None
@@ -18,7 +21,7 @@ def open_file_lines(path):
 
 
 def add_file_line(path, line):
-    for linenum, line in enumerate( fileinput.FileInput(path, inplace=1)):
+    for linenum, line in enumerate(fileinput.FileInput(os.path.join(APP_ROOT, path), inplace=1)):
         if linenum == 0:
             print line
             print line.rstrip()
