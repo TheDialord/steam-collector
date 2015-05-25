90f1486c59cc6e07668a0a79f44e5d848a708efb
Yegor Dia <yegordia@gmail.com>
2015-05-26 02:03:09 +0500

    flask test 2

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-	return flask.jsonify({
-		'success': True,
-		'message': 'Collector works well!'
-		'error': -1
-	})
+    return jsonify({
+        'success': True,
+        'message': 'Collector works well!',
+        'error': -1
+    })
