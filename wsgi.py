ca5079fcf9b67fbd4f13ad94dc1793ea81bfaa8f
Yegor Dia <yegordia@gmail.com>
2015-05-25 23:42:09 +0500

    flask added

--- wsgi.py
+++ wsgi.py
@@ -12,6 +12,10 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
+from app import app
+
+application = app
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
