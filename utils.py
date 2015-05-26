72fe895c850f2203d75ac704b99ceb32f5e91ae3
Yegor Dia <yegordia@gmail.com>
2015-05-26 05:36:43 +0500

    json back

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-import ujson
+import json
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -7,27 +7,27 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def open_file_line(path):
     line = None
     with open(os.path.join(APP_ROOT, path), "r") as f:
-        data = ujson.load(f)
+        data = json.load(f)
         line = data[0]
 
         data = data[1:]
         with open(os.path.join(APP_ROOT, path), 'w+') as _f:
-            ujson.dump(data, _f)
+            json.dump(data, _f)
     return line
 
 
 def open_file_lines(path):
     with open(os.path.join(APP_ROOT, path), "r") as f:
-        data = ujson.load(f)
+        data = json.load(f)
         return data
 
 
 def add_file_line(path, line):
     with open('data.json', 'r') as f:
-        data = ujson.load(f)
+        data = json.load(f)
         data.insert(0, line)
 
     with open('data.json', 'w') as f:
-        ujson.dump(data, f)
+        json.dump(data, f)
         return True
 
